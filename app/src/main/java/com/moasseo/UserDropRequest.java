42ba5bba8ed1c6568a2be5aa83d514decb284842
GomPlay3r <quf2009@naver.com>
2022-09-27 09:34:24 +0900

    비밀번호 변경까지 됐고, 매니패스트 수정해주셈~

---
+++ app/src/main/java/com/moasseo/UserDropRequest.java
@@ -0,0 +1,34 @@
+package com.moasseo;
+
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDropRequest extends StringRequest {
+    //서버 URL 설정 (PHP 파일 연동)
+    final static private String URL = "http://10.0.2.2/capstone/userdrop.php";
+    //final static private String URL = "http://172.111.106.2/inphp/MainLogin.php";
+    private Map<String, String> map;
+
+    public UserDropRequest(String id, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("id", id);  //php에 이름 넘기기
+
+    }
+
+    @Nullable
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+
+}
