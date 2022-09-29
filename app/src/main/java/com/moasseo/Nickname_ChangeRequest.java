5e30dbe509f095e23c61db76570a09d2112b9af5
GomPlay3r <quf2009@naver.com>
2022-09-29 16:02:58 +0900

    닉네임 변경 완료했음

--- app/src/main/java/com/moasseo/Nickname_ChangeRequest.java
+++ app/src/main/java/com/moasseo/Nickname_ChangeRequest.java
@@ -13,19 +13,19 @@ import java.util.Map;
 
 //POST 방식으로 웹 요청할 클래스
 
-public class MainLoginRequest extends StringRequest {
+public class Nickname_ChangeRequest extends StringRequest {
     //서버 URL 설정 (PHP 파일 연동)
-    final static private String URL = "http://10.0.2.2/capstone/MainLogin.php";
+    final static private String URL = "http://10.0.2.2/capstone/nick_change.php";
     //final static private String URL = "http://172.111.106.2/inphp/MainLogin.php";
     private Map<String, String> map;
 
-    public MainLoginRequest(String User_id, String User_pwd, Response.Listener<String> listener) {
-    //public MainLoginRequest(String User_id, Response.Listener<String> listener) {
+    public Nickname_ChangeRequest(String id, String nnm, Response.Listener<String> listener) {
+        //public MainLoginRequest(String User_id, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
-        map.put("User_id", User_id);  //아이디와 비밀번호를 통해 로그인.
-        map.put("User_pwd", User_pwd);
+        map.put("id", id);  //아이디와 비밀번호를 통해 로그인.
+        map.put("nnm", nnm);
 
     }
 
