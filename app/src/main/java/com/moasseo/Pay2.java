f1ecc48dae1c1d108fb17fa987db838a96da01d1
DESKTOP-S9CAM8G\82105 <inwoo9713@gmail.com>
2022-11-20 19:49:52 +0900

    11.20 민석 commit qr 정보 갖고와서 화면에 뿌려주기

--- app/src/main/java/com/moasseo/Pay2.java
+++ app/src/main/java/com/moasseo/Pay2.java
@@ -15,8 +15,9 @@ public class Pay2 extends AppCompatActivity {
     TextView totalpay;
     Button next12;  //확인 버튼
 
-    String nnm; //닉네임을 저장할 변수
+    String nnm, mnm, onm; //닉네임을 저장할 변수
 
+    TextView t1, t2;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +27,16 @@ public class Pay2 extends AppCompatActivity {
         next12 = findViewById(R.id.next12);
 
         Intent intent = getIntent();
+        mnm = intent.getStringExtra("mnm");//시장명
+        onm = intent.getStringExtra("onm");//사장명
         nnm = intent.getStringExtra("nnm");//닉네임
         total = intent.getStringExtra("totalpay");
+
+        t1 = (TextView) findViewById(R.id.textView82);
+        t1.setText(mnm);  //시장명 표시
+
+        t2 = (TextView) findViewById(R.id.textView110);
+        t2.setText(onm + "에게");  //시장명 표시
 
         totalpay.setText(total + "원");
 
