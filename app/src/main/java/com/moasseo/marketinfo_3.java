621288668ea208179e0c43dcf4268a2dd8e4c465
incheol <smu06030>
2022-11-20 18:11:58 +0900

    11월 20일 marketinfo_2,3 java코드 추가 manifast도 추가

---
+++ app/src/main/java/com/moasseo/marketinfo_3.java
@@ -0,0 +1,14 @@
+package com.moasseo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class marketinfo_3 extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.marketinformation3);
+    }
+}
